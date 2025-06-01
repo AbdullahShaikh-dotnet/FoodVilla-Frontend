b2fec56a11eccf785d9d1ad152cd94902f570061
abdullah034 <shaikhabdullah299@gmail.com>
2025-06-01 23:04:52 +0530

    Learn JSX

--- index.js
+++ index.js
@@ -3,18 +3,23 @@ import ReactDOM from "react-dom/client"
 
 
 
-const Parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "Child" }, [
-    React.createElement("h1", { id: "Child1-1" }, "I am Heading"),
-    React.createElement("h2", { id: "Child1-2" }, "I am Heading"),
-    React.createElement("h3", { id: "Child1-3" }, "I am Heading"),
-  ]),
-  React.createElement("div", { id: "Child" }, [
-    React.createElement("h1", { id: "Child2-1" }, "I am Heading"),
-    React.createElement("h2", { id: "Child2-2" }, "I am Heading"),
-    React.createElement("canvas", { id: "Child2-3" }, "I am Heading"),
-  ]),
-]);
+// const Parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "Child" }, [
+//     React.createElement("h1", { id: "Child1-1" }, "I am Heading"),
+//     React.createElement("h2", { id: "Child1-2" }, "I am Heading"),
+//     React.createElement("h3", { id: "Child1-3" }, "I am Heading"),
+//   ]),
+//   React.createElement("div", { id: "Child" }, [
+//     React.createElement("h1", { id: "Child2-1" }, "I am Heading"),
+//     React.createElement("h2", { id: "Child2-2" }, "I am Heading"),
+//     React.createElement("canvas", { id: "Child2-3" }, "I am Heading"),
+//   ]),
+// ]);
+
+ const Parent = <div>
+                    <h1 id="heading-1">Heading From React JS</h1>
+                    <h1 id="heading-2">Heading From React JS 2</h1>
+                </div>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
