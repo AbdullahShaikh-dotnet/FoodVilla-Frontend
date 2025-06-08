b9ce92519804a50915088b29c8d1375172352caf
abdullah034 <shaikhabdullah299@gmail.com>
2025-06-08 20:57:47 +0530

    Used useState to create a State Variable that sync data with ui whenever data change ui reRender

--- src/Component/Body.js
+++ src/Component/Body.js
@@ -1,24 +1,40 @@
 import RestaurantCard from "./RestaurantCard";
 import { ResData } from "../utils/mockdata";
+import { useState } from "react";
 
-const Body = () => (
-    <div className="body">
-        <div className="search">
-            <input type="text" placeholder="Search" />
-        </div>
-            {ResData.restaurants.map((restaurant) => (
-                restaurant.menu.map((item, index) => (
-                    <RestaurantCard key={restaurant.id + index} 
-                    restaurantName={restaurant.name} 
-                    rating={restaurant.rating} 
-                    cuisines={restaurant.cuisine} 
-                    dishName={item.name}
-                    price={item.price}
-                    imageUrl={item.image_url} />
-                ))
-            ))} 
+const Body = () => {
+    
+const [restaurant, setrestaurant] = useState(ResData);
+
+    
+    return (
+  <div className="body">
+    <div className="search">
+      <input type="text" placeholder="Search" />
+      <button
+        className="btn"
+        onClick={() => {
+          const FilteredList = ResData.filter((x) => x.rating > 4.5);
+          setrestaurant(FilteredList);
+        }}
+      >
+        top Rated Restaurant
+      </button>
     </div>
-)
-
+    {restaurant.map((res) =>
+      res.menu.map((item, index) => (
+        <RestaurantCard
+          key={res.id + index}
+          restaurantName={res.name}
+          rating={res.rating}
+          cuisines={res.cuisine}
+          dishName={item.name}
+          price={item.price}
+          imageUrl={item.image_url}
+        />
+      ))
+    )}
+  </div>
+)};
 
 export default Body;
