95acc2b7f50174bf22a69dc1a5c9862b21a3ff75
abdullah034 <shaikhabdullah299@gmail.com>
2025-06-08 20:56:09 +0530

    Clean Mock data

--- src/utils/mockdata.js
+++ src/utils/mockdata.js
@@ -1,62 +1,52 @@
-export const ResData = {
-    "restaurants": [
+export const ResData = [
+  {
+    id: 1,
+    name: "Spicy Delight",
+    cuisine: "Indian",
+    rating: 4.5,
+    location: "Andheri West",
+    menu: [
       {
-        "id": 1,
-        "name": "Spicy Delight",
-        "cuisine": "Indian",
-        "rating": 4.5,
-        "location": "Andheri West",
-        "menu": [
-          {
-            "item_id": 101,
-            "name": "Butter Chicken",
-            "price": 350,
-            "category": "Main Course",
-            "is_veg": false,
-            "image_url": "https://www.licious.in/blog/wp-content/uploads/2020/10/butter-chicken--750x750.jpg"
-          },
-          {
-            "item_id": 102,
-            "name": "Paneer Tikka",
-            "price": 250,
-            "category": "Starter",
-            "is_veg": true,
-            "image_url": "https://www.carolinescooking.com/wp-content/uploads/2021/09/paneer-tikka-featured-pic-sq.jpg"
-          }
-        ]
+        item_id: 101,
+        name: "Butter Chicken",
+        price: 350,
+        category: "Main Course",
+        is_veg: false,
+        image_url: "https://www.licious.in/blog/wp-content/uploads/2020/10/butter-chicken--750x750.jpg"
       },
       {
-        "id": 2,
-        "name": "Sushi World",
-        "cuisine": "Japanese",
-        "rating": 4.8,
-        "location": "Bandra",
-        "menu": [
-          {
-            "item_id": 201,
-            "name": "California Roll",
-            "price": 500,
-            "category": "Sushi",
-            "is_veg": false,
-            "image_url": "https://therecipecritic.com/wp-content/uploads/2023/08/california_roll.jpg"
-          },
-          {
-            "item_id": 202,
-            "name": "Veg Tempura",
-            "price": 400,
-            "category": "Appetizer",
-            "is_veg": true,
-            "image_url": "https://th.bing.com/th/id/OIP.RLvyCCBHdJwsKU6Ih5HiUQHaLH?r=0&rs=1&pid=ImgDetMain"
-          }
-        ]
+        item_id: 102,
+        name: "Paneer Tikka",
+        price: 250,
+        category: "Starter",
+        is_veg: true,
+        image_url: "https://www.carolinescooking.com/wp-content/uploads/2021/09/paneer-tikka-featured-pic-sq.jpg"
       }
-    ],
-    "categories": [
-      "Starter",
-      "Main Course",
-      "Sushi",
-      "Appetizer",
-      "Dessert",
-      "Beverages"
     ]
-}
+  },
+  {
+    id: 2,
+    name: "Sushi World",
+    cuisine: "Japanese",
+    rating: 4.8,
+    location: "Bandra",
+    menu: [
+      {
+        item_id: 201,
+        name: "California Roll",
+        price: 500,
+        category: "Sushi",
+        is_veg: false,
+        image_url: "https://therecipecritic.com/wp-content/uploads/2023/08/california_roll.jpg"
+      },
+      {
+        item_id: 202,
+        name: "Veg Tempura",
+        price: 400,
+        category: "Appetizer",
+        is_veg: true,
+        image_url: "https://th.bing.com/th/id/OIP.RLvyCCBHdJwsKU6Ih5HiUQHaLH?r=0&rs=1&pid=ImgDetMain"
+      }
+    ]
+  }
+];
