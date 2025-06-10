692d39fdfddf98404f9033574279b09e9749479c
Abdullah Remort PC <shaikhabdullah299@gmail.com>
2025-06-10 12:36:03 +0530

    Format and Re Design

--- src/Component/RestaurantCard.js
+++ src/Component/RestaurantCard.js
@@ -3,11 +3,13 @@ const RestaurantCard = (prop) => {
   return (
     <div className="restaurant-card">
       <img className="restaurant-image" src={imageUrl} alt={dishName} />
-      <h5>Restaurant Name : {restaurantName}</h5>
-      <h5>Rating : {rating}</h5>
-      <h5>Cuisines : {cuisines}</h5>
-      <h5>Dish Name : {dishName}</h5>
-      <h5>Price : {price}</h5>
+      <div className="restaurant-info">
+        <h3>{restaurantName}</h3>
+        <div className="rating">⭐ {rating}</div>
+        <p>{cuisines}</p>
+        <p><strong>{dishName}</strong></p>
+        <p>₹{price}</p>
+      </div>
     </div>
   );
 };
