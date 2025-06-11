931fb71b2d8bc387acd3b3ce46d93db5875586a4
Abdullah Remort PC <shaikhabdullah299@gmail.com>
2025-06-11 18:56:24 +0530

    Implemented Client Side Routing

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,19 +1,34 @@
 import { LOGO_URL } from "../utils/constant";
+import { useState, useEffect } from "react";
+import { Link } from "react-router";
 
-const Header = () => (
+const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
     <div className="heading">
-        <div className="logo-container">
-            <img src={LOGO_URL} alt="logo" />
-        </div>
-        <div className="nav-items">
-            <ul>
-                <li className="nav-items-active">Home</li>
-                <li>About</li>
-                <li>Contact</li>
-                <li>Cart</li>
-            </ul>
-        </div>
+      <div className="logo-container">
+        <img src={LOGO_URL} alt="logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <Link to="/"><li className="nav-items-active">
+            Home</li></Link>
+          <Link to="/About"><li>About</li></Link>
+          <Link to="/Contact"><li>Contact</li></Link>
+          <li>Cart</li>
+          <li>
+            <div className="login-container">
+              <button
+                className={isLoggedIn ? "danger-button" : "login-button"}
+                onClick={() => setIsLoggedIn(!isLoggedIn)}>
+                {isLoggedIn ? "Logout" : "Login"}
+              </button>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
-);
+  );
+};
 
 export default Header;
