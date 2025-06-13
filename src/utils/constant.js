6509137fe0771669dff48e39e15c8dd15b49b1f1
abdullah034 <shaikhabdullah299@gmail.com>
2025-06-14 01:49:29 +0530

    Shift Compont Hardcoded Endpoint URL into Constant File

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,5 @@
 export const LOGO_URL = "../src/assets/logo.png";
+
+export const RESTAURANT_MENU_API_ENDPOINT = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9716&lng=77.5946&restaurantId=";
+
+export const RESTAURANT_LIST_API_ENDPOINT = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559";
