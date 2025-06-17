4b93205abd2ef0b876c9fdc9d9103429ba267071
Abdullah Remort PC <shaikhabdullah299@gmail.com>
2025-06-17 18:46:08 +0530

    Added Github API Call

--- src/Component/AboutUserClass.js
+++ src/Component/AboutUserClass.js
@@ -6,26 +6,48 @@ class AboutUserClass extends React.Component {
 
     this.state = {
       count: 0,
+      userInfo: {},
+      projectDetails: {}
     };
   }
 
+  async componentDidMount() {
+    const Data = await fetch("https://api.github.com/users/AbdullahShaikh-dotnet");
+    const Json = await Data.json();
+    
+    const RepositoryData = await fetch(Json.repos_url);
+    const ReposJson = await RepositoryData.json();
+
+    this.setState({
+      userInfo: Json,
+      projectDetails: ReposJson
+    })
+
+  }
+
+
   render() {
-    const { name, title, description, email } = this.props;
+    const { name, login, avatar_url, html_url, location } = this.state.userInfo;
     return (
       <div className="about-user-container">
         <div className="user-card">
           <img
-            src="https://randomuser.me/api/portraits/men/32.jpg"
+            src={avatar_url}
             alt="User Avatar"
             className="user-avatar"
           />
           <h2 className="user-name">{name}</h2>
-          <h4 className="user-title">{title}</h4>
+          <p>{location}</p>
+          <h4 className="user-title">Software Developer {login}</h4>
           <p className="user-description">
             Passionate about building beautiful and performant web applications.
             Loves React, UI/UX, and learning new tech!
           </p>
-          <a href={`mailto:${email}`} className="contact-button">Contact</a>
+          <a className="contact-button" style={{ backgroundColor: "white" }} href={html_url} target="_blank">
+            <img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" width="25px" height="25px"></img>
+          </a>
+          <a href={`mailto:shaikhabdullah299@gmail.com`} className="contact-button">
+            Contact</a>
           <a
             onClick={() => {
               this.setState({
