18702874b5fac7ab2973cccc8964426c9fefc2b9
Abdullah Remort PC <shaikhabdullah299@gmail.com>
2025-06-30 18:43:24 +0530

    Connected Login Componet with Link

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -22,19 +22,17 @@ const Header = () => {
             <li className="flex items-center text-sm">
               <span>{useOnlineStatus() ? "Online" : "Offline"}</span>
               <span
-                className={`ml-2 h-2.5 w-2.5 rounded-full ${
-                  useOnlineStatus() ? "bg-green-500" : "bg-gray-400"
-                }`}
+                className={`ml-2 h-2.5 w-2.5 rounded-full ${useOnlineStatus() ? "bg-green-500" : "bg-gray-400"
+                  }`}
               ></span>
             </li>
             <li>
               <Link
                 to="/"
-                className={`py-2 border-b-2 ${
-                  currentPage === "/"
-                    ? "text-orange-500 border-orange-500"
-                    : "border-transparent"
-                } hover:text-orange-500 hover:border-orange-500 transition-colors duration-300`}
+                className={`py-2 border-b-2 ${currentPage === "/"
+                  ? "text-orange-500 border-orange-500"
+                  : "border-transparent"
+                  } hover:text-orange-500 hover:border-orange-500 transition-colors duration-300`}
               >
                 Home
               </Link>
@@ -42,11 +40,10 @@ const Header = () => {
             <li>
               <Link
                 to="/About"
-                className={`py-2 border-b-2 ${
-                  currentPage === "/About"
-                    ? "text-orange-500 border-orange-500"
-                    : "border-transparent"
-                } hover:text-orange-500 hover:border-orange-500 transition-colors duration-300`}
+                className={`py-2 border-b-2 ${currentPage === "/About"
+                  ? "text-orange-500 border-orange-500"
+                  : "border-transparent"
+                  } hover:text-orange-500 hover:border-orange-500 transition-colors duration-300`}
               >
                 About
               </Link>
@@ -54,11 +51,10 @@ const Header = () => {
             <li>
               <Link
                 to="/Contact"
-                className={`py-2 border-b-2 ${
-                  currentPage === "/Contact"
-                    ? "text-orange-500 border-orange-500"
-                    : "border-transparent"
-                } hover:text-orange-500 hover:border-orange-500 transition-colors duration-300`}
+                className={`py-2 border-b-2 ${currentPage === "/Contact"
+                  ? "text-orange-500 border-orange-500"
+                  : "border-transparent"
+                  } hover:text-orange-500 hover:border-orange-500 transition-colors duration-300`}
               >
                 Contact
               </Link>
@@ -86,7 +82,7 @@ const Header = () => {
               </Link>
             </li>
           </ul>
-          <button
+          {/* <button
             className={`px-6 py-2 rounded-md font-bold ${
               isLoggedIn
                 ? "bg-red-500 hover:bg-red-600 text-white"
@@ -95,7 +91,13 @@ const Header = () => {
             onClick={() => setIsLoggedIn(!isLoggedIn)}
           >
             {isLoggedIn ? "Logout" : "Login"}
-          </button>
+          </button> */}
+
+          <Link to="/Login" className={`py-2 border-b-2 hover:text-orange-500 hover:border-orange-500 transition-colors duration-300 font-extrabold bg-white text-gray-500 
+          ${currentPage === '/Login' ? "text-orange-500 border-orange-500"
+            : "border-transparent"}`}>
+            Login
+          </Link>
         </div>
       </nav>
     </header>
