6af518422253be6e7cb68718aa69de1a184ae858
abdullah034 <shaikhabdullah299@gmail.com>
2025-07-13 23:36:49 +0530

    Added Proper Logic for Store Remove Reducer

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -1,25 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
-    name: 'cart',
-    initialState: {
-        items: []
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addItems: (state, action) => {
+      state.items.push(action.payload);
     },
-    reducers: {
-        addItems: (state, action) => {
-            state.items.push(action.payload);
-        },
 
-        removeItem: (state, action) => {
-            state.items.pop();
-        },
+    removeItem: (state, action) => {
+      const id = action.payload;
+      state.items = state.items.filter(item => item.card.info.id !== id);
+    },
 
-        clearCart: (state, action) => {
-            state.items.length = 0;
-        }
-    }
+    clearCart: (state, action) => {
+      state.items.length = 0;
+    },
+
+    // Increment Item Quantity
+
+    // Decrement Item Quantity
+  },
 });
-
 
 export const { addItems, removeItem, clearCart } = cartSlice.actions;
 
