b344e8b71609477996de9b3c0cd5eff70e9078f7
abdullah034 <shaikhabdullah299@gmail.com>
2025-07-15 00:06:37 +0530

    Added Increment and Decrement Cart Count

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -7,24 +7,51 @@ const cartSlice = createSlice({
   },
   reducers: {
     addItems: (state, action) => {
-      state.items.push(action.payload);
+      let data = action.payload;
+      state.items.push(data);
     },
 
     removeItem: (state, action) => {
       const id = action.payload;
-      state.items = state.items.filter(item => item.card.info.id !== id);
+      state.items = state.items.filter((item) => item.card.info.id !== id);
     },
 
-    clearCart: (state, action) => {
+    clearCart: (state) => {
       state.items.length = 0;
     },
 
     // Increment Item Quantity
+    incrementQuantity: (state, action) => {
+      const id = action.payload;
+      const item = state.items.find((item) => item.card.info.id === id);
+      if (item) {
+        item.quantity = (item.quantity || 1) + 1;
+      }
+    },
 
     // Decrement Item Quantity
+    decrementQuantity: (state, action) => {
+      const id = action.payload;
+      const item = state.items.find((item) => item.card.info.id === id);
+      if (!item) return;
+
+      if (item.quantity === 1) {
+        state.items = state.items.filter(
+          (i) => i.card.info.id !== item.card.info.id
+        );
+      } else {
+        item.quantity = item.quantity - 1;
+      }
+    },
   },
 });
 
-export const { addItems, removeItem, clearCart } = cartSlice.actions;
+export const {
+  addItems,
+  removeItem,
+  clearCart,
+  incrementQuantity,
+  decrementQuantity,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
